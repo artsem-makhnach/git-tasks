44779c3cf8fa1bfca627e222cead520bba2fad34
Lakhtenkov-iv <lakhtenkov@gmail.com>
2017-11-09 15:39:16 +0300

    Feature/Restoring Jenkins config if exists

--- jenkins_setup.sh
+++ jenkins_setup.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
+JENKINS_HOME="/opt/jenkins/master"
+JENKINS_DIR="/opt/jenkins/bin"
+
 #Installing dependencies
 yum -y install git > /dev/null 2>&1
 
 #Script for Installation and Configuration Jenkins
-mkdir -p /opt/jenkins/master
-mkdir -p /opt/jenkins/bin
-wget -qO /opt/jenkins/bin/jenkins.war http://mirrors.jenkins.io/war-stable/latest/jenkins.war
+mkdir -p $JENKINS_HOME
+mkdir -p $JENKINS_DIR
+wget -qO $JENKINS_DIR/jenkins.war http://mirrors.jenkins.io/war-stable/latest/jenkins.war
 
 #Restoring Jenkins config if exists
 if [ -e /vagrant/jenkins_backup.tar.gz ]&&[ -e /vagrant/plugins.txt ]; then
-   tar -C /opt/jenkins/master -xzf /vagrant/jenkins_backup.tar.gz
+   tar -C $JENKINS_HOME -xzf /vagrant/jenkins_backup.tar.gz
    while read line ; do
-      curl -L --silent --output /root/.jenkins/plugins/$line.hpi https://updates.jenkins-ci.org/latest/$line.hpi
+      curl -L --silent --output $JENKINS_HOME/plugins/$line.hpi https://updates.jenkins-ci.org/latest/$line.hpi
    done < /vagrant/plugins.txt
 fi
 
@@ -25,8 +28,8 @@ cat > /etc/systemd/system/jenkins.service <<-EOF
 	Description=Jenkins Daemon
 	
 	[Service]
-	Environment=JENKINS_HOME=/opt/jenkins/master
-	Environment=JENKINS_DIR=/opt/jenkins/bin
+	Environment=JENKINS_HOME=$JENKINS_HOME
+	Environment=JENKINS_DIR=$JENKINS_DIR
 	ExecStart=/usr/bin/java -jar /opt/jenkins/master/jenkins.war
 	User=jenkins
 	

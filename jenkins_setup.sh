8ef7bfdfb9c08aa2dc581907099e978422c9cb5d
Lakhtenkov-iv <lakhtenkov@gmail.com>
2017-11-09 15:28:21 +0300

    Jenkins systemd ExecStart path correction

--- jenkins_setup.sh
+++ jenkins_setup.sh
@@ -8,6 +8,15 @@ mkdir -p /opt/jenkins/master
 mkdir -p /opt/jenkins/bin
 wget -qO /opt/jenkins/bin/jenkins.war http://mirrors.jenkins.io/war-stable/latest/jenkins.war
 
+#Restoring Jenkins config if exists
+if [ -e /vagrant/jenkins_backup.tar.gz ]&&[ -e /vagrant/plugins.txt ]; then
+   tar -C /opt/jenkins/master -xzf /vagrant/jenkins_backup.tar.gz
+   while read line ; do
+      curl -L --silent --output /root/.jenkins/plugins/$line.hpi https://updates.jenkins-ci.org/latest/$line.hpi
+   done < /vagrant/plugins.txt
+fi
+
+
 useradd jenkins
 chown -R jenkins: /opt/jenkins/
 
@@ -18,7 +27,7 @@ cat > /etc/systemd/system/jenkins.service <<-EOF
 	[Service]
 	Environment=JENKINS_HOME=/opt/jenkins/master
 	Environment=JENKINS_DIR=/opt/jenkins/bin
-	ExecStart=/usr/bin/java -jar /opt/jenkins/jenkins.war
+	ExecStart=/usr/bin/java -jar /opt/jenkins/master/jenkins.war
 	User=jenkins
 	
 	[Install]

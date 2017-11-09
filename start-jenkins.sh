4734c82c82b06e3a9c711691da86ee082ebb9dd9
artsem-makhnach <artsem_makhnach@epam.com>
2017-11-09 15:14:36 +0300

    export JENKINS_HOME in start script

--- start-jenkins.sh
+++ start-jenkins.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-JENKINS_DIR=/opt/jenkins/bin
+export JENKINS_DIR=/opt/jenkins/bin
+export JENKINS_HOME=/opt/jenkins/master
 JENKINS_LOG=/opt/jenkins/bin/jenkins.log
 
 nohup java -jar $JENKINS_DIR/jenkins.war > $JENKINS_LOG 2>&1 &

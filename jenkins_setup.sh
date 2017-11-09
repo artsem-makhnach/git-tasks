1a2bcd67319f45e68b365326fb549c55e8282446
Lakhtenkov-iv <lakhtenkov@gmail.com>
2017-11-09 14:18:12 +0300

    Created jenkins setup and configuration file jenkins_setup.sh

---
+++ jenkins_setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#Installing dependencies
+yum -y install git > /dev/null 2>&1
+
+#Script for Installation and Configuration Jenkins
+mkdir -p /opt/jenkins/master
+mkdir -p /opt/jenkins/bin
+wget -qO /opt/jenkins/jenkins.war http://mirrors.jenkins.io/war-stable/latest/jenkins.war
+
+useradd jenkins
+chown -R jenkins: /opt/jenkins/
+
+cat > /etc/systemd/system/jenkins.service <<-EOF
+	[Unit]
+	Description=Jenkins Daemon
+	
+	[Service]
+	Environment=JENKINS_HOME=/opt/jenkins/master
+	Environment=JENKINS_DIR=/opt/jenkins/bin
+	ExecStart=/usr/bin/java -jar /opt/jenkins/jenkins.war
+	User=jenkins
+	
+	[Install]
+	WantedBy=multi-user.target
+EOF
+
+systemctl enable jenkins
+systemctl start jenkins
